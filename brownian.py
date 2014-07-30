a9fc9152974a5c83047380e0b241dcbbf8d6fd46
Antonino Ingargiola <tritemio@gmail.com>
2014-07-30 12:55:56 -0700

    Move function merge_ts_da from notebook to brownian.py

--- brownian.py
+++ brownian.py
@@ -566,6 +566,25 @@ def load_simulation(fname):
                     S.timestamps = S.ts_group._f_get_child(name)
     return S
 
+def merge_ts_da(ts_d, ts_par_d, ts_a, ts_par_a):
+    """Merge donor and acceptor timestamps and particle arrays.
+
+    Parameters:
+        ts_d (array): donor timestamp array
+        ts_par_d (array): donor particles array
+        ts_a (array): acceptor timestamp array
+        ts_par_a (array): acceptor particles array
+
+    Returns:
+        Arrays: timestamps, acceptor bool mask, timestamp particle
+    """
+    ts = np.hstack([ts_d, ts_a])
+    ts_par = np.hstack([ts_par_d, ts_par_a])
+    a_ch = np.hstack([np.zeros(ts_d.shape[0], dtype=bool),
+                      np.ones(ts_a.shape[0], dtype=bool)])
+    index_sort = ts.argsort()
+    return ts[index_sort], a_ch[index_sort], ts_par[index_sort]
+
 ##
 # Functions to manage/merge multiple simulations
 #
