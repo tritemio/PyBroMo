88936b22fbff77d02f3c68ab4071b955b4364966
Antonino Ingargiola <tritemio@gmail.com>
2014-07-31 10:45:30 -0700

    Add compress parameter to hdf5.print_attrs

--- utils/hdf5.py
+++ utils/hdf5.py
@@ -9,7 +9,7 @@ pytables HDF5 files.
 """
 
 
-def print_attrs(data_file, node_name='/', which='user'):
+def print_attrs(data_file, node_name='/', which='user', compress=False):
     """Print the HDF5 attributes for `node_name`.
 
     Parameters:
@@ -19,12 +19,17 @@ def print_attrs(data_file, node_name='/', which='user'):
         which (string): Valid values are 'user' for user-defined attributes,
             'sys' for pytables-specific attributes and 'all' to print both
             groups of attributes. Default 'user'.
+        compress (bool): if True displays at most a line for each attribute.
+            Default False.
     """
     node = data_file.get_node(node_name)
     print 'List of attributes for:\n  %s\n' % node
     for attr in node._v_attrs._f_list():
         print '\t%s' % attr
-        print "\t    %s" % repr(node._v_attrs[attr])
+        attr_content = repr(node._v_attrs[attr])
+        if compress:
+            attr_content = attr_content.split('\n')[0]
+        print "\t    %s" % attr_content
 
 def print_children(data_file, group='/'):
     """Print all the sub-groups in `group` and leaf-nodes children of `group`.
