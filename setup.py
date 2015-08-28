c868a357f5891fd365d06621cead37d178ddbdf5
Antonino Ingargiola <tritemio@gmail.com>
2015-08-27 20:24:19 -0700

    Packaging: move py files in subfolder and add setup.py

---
+++ setup.py
@@ -0,0 +1,50 @@
+from setuptools import setup
+import versioneer
+
+project_name = 'pybromo'
+
+## Configure versioneer
+versioneer.VCS = 'git'
+versioneer.versionfile_source = project_name + '/_version.py'
+versioneer.versionfile_build = project_name + '/_version.py'
+versioneer.tag_prefix = ''  # tags are like 1.2.0
+versioneer.parentdir_prefix = project_name + '-'
+
+
+## Metadata
+long_description = """
+PyBroMo
+==========
+
+A brownian motion and timestamps/lifetimes simulator for freely diffusing
+fluorescent particles (or dyes) under confocal excitation. If can be used to
+accurately simulate confocal single-molecule FRET experiments.
+
+"""
+
+setup(name = project_name,
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
+      author = 'Antonino Ingargiola',
+      author_email = 'tritemio@gmail.com',
+      url = 'https://github.com/tritemio/PyBroMo',
+      download_url = 'https://github.com/tritemio/PyBroMo',
+      install_requires = ['numpy', 'setuptools', 'tables', 'matplotlib',
+                          'future'],
+      license = 'GPLv2',
+      description = ('Simulator for confocal single-molecule fluorescence '
+                     'experiments.'),
+      long_description = long_description,
+      platforms = ('Windows', 'Linux', 'Mac OS X'),
+      classifiers=['Intended Audience :: Science/Research',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3.4',
+                   'Topic :: Scientific/Engineering',
+                   ],
+      packages = ['pybromo'],
+      package_data = {'pybromo': ['psf_data/*']},
+      keywords = ('single-molecule FRET smFRET biophysics confocal '
+                  'freely-diffusing brownian-motion simulator'),
+      )
