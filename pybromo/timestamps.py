daade5629b34aa5164d7aad1425c1df90dcaa726
Antonino Ingargiola <tritemio@gmail.com>
2015-09-15 10:38:13 -0700

    Add helper functions for timestamps simulation of mixtures

--- pybromo/timestamps.py
+++ pybromo/timestamps.py
@@ -9,6 +9,7 @@ This module contains functions to work with timestamps.
 """
 
 import numpy as np
+import time
 
 
 def merge_da(ts_d, ts_par_d, ts_a, ts_par_a):
@@ -29,3 +30,109 @@ def merge_da(ts_d, ts_par_d, ts_a, ts_par_a):
                       np.ones(ts_a.shape[0], dtype=bool)])
     index_sort = ts.argsort()
     return ts[index_sort], a_ch[index_sort], ts_par[index_sort]
+
+##
+#  Timestamp simulation definitions
+#
+
+def em_rates_from_E_DA(em_rate_tot, E_values):
+    """Donor and Acceptor emission rates from total emission rate and E (FRET).
+    """
+    E_values = np.asarray(E_values)
+    em_rates_a = E_values * em_rate_tot
+    em_rates_d = em_rate_tot - em_rates_a
+    return em_rates_d, em_rates_a
+
+def em_rates_from_E_unique(em_rate_tot, E_values):
+    """Array of unique emission rates for given total emission and E (FRET).
+    """
+    em_rates_d, em_rates_a = em_rates_DA_from_E(em_rate_tot, E_values)
+    return np.unique(np.hstack([em_rates_d, em_rates_a]))
+
+def em_rates_from_E_DA_mix(em_rate_tot1, em_rate_tot2, E_values1, E_values2):
+    """D and A emission rates for two populations.
+    """
+    em_rates_d1, em_rates_a1 = em_rates_DA_from_E(em_rate_tot1, E_values1)
+    em_rates_d2, em_rates_a2 = em_rates_DA_from_E(em_rate_tot2, E_values2)
+    return em_rates_d1, em_rates_a1, em_rates_d2, em_rates_a2
+
+def populations_diff_coeff(particles, num_pop1, num_pop2):
+    """Diffusion coefficients of the two specified populations.
+    """
+    D_counts = particles.diffusion_coeff_counts
+    if len(D_counts) == 1:
+        D1 = D2 = D_counts[0][0]
+    elif len(D_counts) == 2:
+        # Multiple diffusion coefficients
+        (D1, _num_pop1), (D2, _num_pop2) = D_counts
+        assert _num_pop1 == num_pop1
+        assert _num_pop2 == num_pop2
+    return D1, D2
+
+def populations_slices(particles, num_pop1, num_pop2):
+    """2-tuple of slices for selection of two populations.
+    """
+    return slice(0, num_pop1), slice(num_pop1, num_pop1 + num_pop2)
+
+class MixtureSimulation:
+    """Simulate timestamps for a mixture of two populations."""
+    def __init__(self, S, params):
+        self.S = S
+        for k, v in params.items():
+            setattr(self, k, v)
+
+        rates = em_rates_DA_from_E_mix(self.em_rate_tot1, self.em_rate_tot2,
+                                       self.E1, self.E2)
+        self.em_rate_d1, self.em_rate_a1 = rates[:2]
+        self.em_rate_d2, self.em_rate_a2 = rates[2:]
+        self.bg_rates = [self.bg_rate_a, self.bg_rate_d]
+        self.pop_slices = populations_slices(S.particles,
+                                             self.num_pop1, self.num_pop2)
+        self.D1, self.D2 = populations_diff_coeff(S.particles,
+                                                  self.num_pop1, self.num_pop2)
+        self.traj_filename = S.store.filepath.name
+
+    def __str__(self):
+        s = """
+        Timestamps simulation: Mixture
+        ------------------------------
+
+        Trajectories file:
+            {self.traj_filename}
+
+        Population1:
+            # particles:        {self.num_pop1:7}
+            D                   {self.D1} m^2/s
+            Peak emission rate: {self.em_rate_tot1:,.0f} cps
+            FRET efficiency:    {self.E1:7.1%}
+
+        Population2:
+            # particles:        {self.num_pop2:7}
+            D                   {self.D2} m^2/s
+            Peak emission rate: {self.em_rate_tot2:,.0f} cps
+            FRET efficiency:    {self.E2:7.1%}
+
+        Background:
+            Donor:              {self.bg_rate_d:7,} cps
+            Acceptor:           {self.bg_rate_a:7,} cps
+        """.format(self=self)
+        return s
+
+    def summarize(self):
+        print(str(self), flush=True)
+
+    def run(run(rs, overwrite=True, path='./'):
+        header = ' - Mixture Simulation:'
+        print('%s Donor timestamps - %s' % (header, time.ctime(), flush=True)
+        self.S.simulate_timestamps_mix(
+            populations = self.pop_slices,
+            max_rates = (self.em_rate_d1, em_rate_d2),
+            bg_rate = self.bg_rate_d,
+            rs=rs, overwrite=overwrite, path=path)
+        print('%s Acceptor timestamps - %s' % (header, time.ctime(), flush=True)
+        self.S.simulate_timestamps_mix(
+            populations = self.pop_slices,
+            max_rates = (self.em_rate_a1, em_rate_a2),
+            bg_rate = self.bg_rate_a,
+            rs=rs, overwrite=overwrite, path=path)
+        print('%s Completed. %s' % (header, time.ctime(), flush=True)
