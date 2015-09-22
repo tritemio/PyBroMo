4e58d01ed46c2304b33a19a897e6c968a072b1cb
Antonino Ingargiola <tritemio@gmail.com>
2015-09-22 10:32:11 -0700

    ENH: allow populations with less particles than the one in trajectories

--- pybromo/timestamps.py
+++ pybromo/timestamps.py
@@ -70,12 +70,17 @@ def populations_diff_coeff(particles, populations):
     """
     D_counts = particles.diffusion_coeff_counts
     if len(D_counts) == 1:
-        D_counts = [D_counts[0]] * len(populations)
+        pop_sizes = [pop.stop - pop.start for pop in populations]
+        assert D_counts[0][1] >= sum(pop_sizes)
+        D_counts = [(D_counts[0][0], ps) for ps in pop_sizes]
 
     D_list = []
+    D_pop_start = 0  # start index of diffusion-based populations
     for pop, (D, counts) in zip(populations, D_counts):
         D_list.append(D)
-        assert (pop.stop - pop.start) == counts
+        assert pop.start >= D_pop_start
+        assert pop.stop <= D_pop_start + counts
+        D_pop_start += counts
     return D_list
 
 def populations_slices(particles, num_pop_list):
