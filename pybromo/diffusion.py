b5b2b987f659cb402d8163f57dafdc37800d6fbe
Antonino Ingargiola <tritemio@gmail.com>
2015-09-25 14:16:53 -0700

    FIX: fix Particles.positions

--- pybromo/diffusion.py
+++ pybromo/diffusion.py
@@ -137,8 +137,7 @@ class Particles(object):
     @property
     def positions(self):
         """Start positions for all the particles. Shape (N, 3, 1)."""
-        return (np.vstack([p.r0 for p in self.particles])
-                         .reshape(len(self), 3, 1))
+        return np.vstack([p.r0 for p in self]).reshape(len(self), 3, 1)
 
     @property
     def diffusion_coeff(self):
