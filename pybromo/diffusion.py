f4ea75647f6c4195b85650ac3dde1bea761f32e2
Antonino Ingargiola <tritemio@gmail.com>
2015-09-25 13:47:53 -0700

    Add method for single-step timestamps simulation.

--- pybromo/diffusion.py
+++ pybromo/diffusion.py
@@ -791,6 +791,113 @@ class ParticlesSimulation(object):
         self._timestamps.attrs['last_random_state'] = rs.get_state()
         self.ts_store.h5file.flush()
 
+    def simulate_timestamps_mix2(self, max_rates, populations, bg_rate,
+                                rs=None, seed=1, chunksize=2**16,
+                                comp_filter=None, overwrite=False,
+                                skip_existing=False, scale=10,
+                                path='./', t_chunksize=2**19, timeslice=None):
+        """Compute timestamps for a mixture of 2 populations.
+
+        The results are saved to disk and accessible as pytables arrays in
+        `.timestamps` and `.tparticles`.
+        The background generated timestamps are assigned a
+        conventional particle number (last particle index + 1).
+
+        Arguments:
+            max_rates (list): list of the peak max emission rate for each
+                population.
+            populations (list of slices): slices to `self.particles`
+                defining each population.
+            bg_rate (float, cps): rate for a Poisson background process
+            rs (RandomState object): random state object used as random number
+                generator. If None, use a random state initialized from seed.
+            seed (uint): when `rs` is None, `seed` is used to initialize the
+                random state, otherwise is ignored.
+            chunksize (int): chunk size used for the on-disk timestamp array
+            comp_filter (tables.Filter or None): compression filter to use
+                for the on-disk `timestamps` and `tparticles` arrays.
+                If None use default compression.
+            overwrite (bool): if True, overwrite any pre-existing timestamps
+                array. If False, never overwrite. The outcome of simulating an
+                existing array is controlled by `skip_existing` flag.
+            skip_existing (bool): if True, skip simulation if the same
+                timestamps array is already present.
+            scale (int): `self.t_step` is multiplied by `scale` to obtain the
+                timestamps units in seconds.
+            path (string): folder where to save the data.
+            timeslice (float or None): timestamps are simulated until
+                `timeslice` seconds. If None, simulate until `self.t_max`.
+        """
+        self.open_store_timestamp(chunksize=chunksize, path=path)
+        rs = self._get_group_randomstate(rs, seed, self.ts_group)
+        if t_chunksize is None:
+            t_chunksize = 2**19
+        timeslice_size = self.n_samples
+        if timeslice is not None:
+            timeslice_size = timeslice // self.t_step
+
+        name = self._get_ts_name_mix(max_rates, populations, bg_rate, rs=rs)
+        kw = dict(name=name, clk_p=self.t_step / scale,
+                  max_rates=max_rates, bg_rate=bg_rate, populations=populations,
+                  num_particles=self.num_particles,
+                  bg_particle=self.num_particles,
+                  overwrite=overwrite, chunksize=chunksize)
+        if comp_filter is not None:
+            kw.update(comp_filter=comp_filter)
+        try:
+            self._timestamps, self._tparticles = (self.ts_store
+                                                  .add_timestamps(**kw))
+        except ExistingArrayError as e:
+            if skip_existing:
+                print(' - Skipping already present timestamps array.')
+                return
+            else:
+                raise e
+
+        self.ts_group._v_attrs['init_random_state'] = rs.get_state()
+        self._timestamps.attrs['init_random_state'] = rs.get_state()
+        self._timestamps.attrs['PyBroMo'] = __version__
+        self._timestamps.attrs['Diffusion'] = 1
+
+        par_start_pos = self.particles.positions
+
+        # Load emission in chunks, and save only the final timestamps
+        bg_rates = [None] * (len(max_rates) - 1) + [bg_rate]
+        for i_start, i_end in iter_chunk_index(timeslice_size, t_chunksize):
+            _, em_chunk = self._sim_trajectories(timeslice_size, par_start_pos,
+                                                 rs,
+                                                 total_emission=False,
+                                                 save_pos=False, radial=False,
+                                                 wrap_func=wrap_mirror)
+
+            # Loop for each population
+            ts_chunk_pop_list, par_index_chunk_pop_list = [], []
+            for rate, pop, bg in zip(max_rates, populations, bg_rates):
+                em_chunk_pop = em_chunk[pop]
+                ts_chunk_pop, par_index_chunk_pop = self._sim_timestamps(
+                    rate, bg, em_chunk_pop, i_start, ip_start=pop.start,
+                    rs=rs, scale=scale, sort=False)
+
+                ts_chunk_pop_list.append(ts_chunk_pop)
+                par_index_chunk_pop_list.append(par_index_chunk_pop)
+
+            # Merge populations
+            times_chunk_s = np.hstack(ts_chunk_pop_list)
+            par_index_chunk_s = np.hstack(par_index_chunk_pop_list)
+
+            # Sort timestamps inside the merged chunk
+            index_sort = times_chunk_s.argsort(kind='mergesort')
+            times_chunk_s = times_chunk_s[index_sort]
+            par_index_chunk_s = par_index_chunk_s[index_sort]
+
+            # Save sorted timestamps (suffix '_s') and corresponding particles
+            self._timestamps.append(times_chunk_s)
+            self._tparticles.append(par_index_chunk_s)
+
+        # Save current random state so it can be resumed in the next session
+        self.ts_group._v_attrs['last_random_state'] = rs.get_state()
+        self._timestamps.attrs['last_random_state'] = rs.get_state()
+        self.ts_store.h5file.flush()
 
 def sim_timetrace(emission, max_rate, t_step):
     """Draw random emitted photons from Poisson(emission_rates).
