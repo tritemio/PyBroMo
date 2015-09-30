007e70b9885db9541d3fe6033ba09db7313be723
Antonino Ingargiola <tritemio@gmail.com>
2015-09-29 23:47:19 -0700

    Add run_da() method

--- pybromo/timestamps.py
+++ pybromo/timestamps.py
@@ -109,7 +109,7 @@ class TimestapSimulation:
 
     - `bg_rate_d`, `bg_rate_a`
 
-    3. Scalars (optional)::
+    3. Scalars (optional):
 
     - `timeslice`
 
@@ -236,6 +236,32 @@ class TimestapSimulation:
             bg_rate = self.bg_rate_a,
             **kwargs)
         print('%s Completed. %s' % (header, ctime()), flush=True)
+
+    def run_da(self, rs, overwrite=True, skip_existing=False, path=None,
+               chunksize=None):
+        """Compute timestamps for current populations."""
+        if path is None:
+            path = str(self.S.store.filepath.parent)
+        kwargs = dict(rs=rs, overwrite=overwrite, path=path,
+                      timeslice=self.timeslice, skip_existing=skip_existing)
+        if chunksize is not None:
+            kwargs['chunksize'] = chunksize
+        header = ' - Mixture Simulation:'
+
+        # Donor timestamps hash is from the input RandomState
+        self.hash_d = hash_(rs.get_state())[:6]  # needed by merge_da()
+        self.hash_a = self.hash_d
+        print('%s Donor + Acceptor timestamps - %s' %
+              (header, ctime()), flush=True)
+        self.S.simulate_timestamps_mix_da(
+            max_rates_d = self.em_rates_d,
+            max_rates_a = self.em_rates_a,
+            populations = self.populations,
+            bg_rate_d = self.bg_rate_d,
+            bg_rate_a = self.bg_rate_a,
+            **kwargs)
+        print('\n%s Completed. %s' % (header, ctime()), flush=True)
+
 
     @property
     def name_timestamps_d(self):
