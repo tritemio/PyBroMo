f0c0a724a9ea5099d561e33aff45b1d668ea1180
Antonino Ingargiola <tritemio@gmail.com>
2017-10-24 11:22:11 -0700

    Fix usage of `-` on bool arrays with recent numpy

--- pybromo/timestamps.py
+++ pybromo/timestamps.py
@@ -289,7 +289,7 @@ class TimestapSimulation:
         ts_a, ts_par_a = self.S.get_timestamps_part(self.name_timestamps_a)
         ts, a_ch, part = merge_da(ts_d, ts_par_d, ts_a, ts_par_a)
         assert a_ch.sum() == ts_a.shape[0]
-        assert (-a_ch).sum() == ts_d.shape[0]
+        assert (~a_ch).sum() == ts_d.shape[0]
         assert a_ch.size == ts_a.shape[0] + ts_d.shape[0]
         self.ts, self.a_ch, self.part = ts, a_ch, part
         self.clk_p = ts_d.attrs['clk_p']
